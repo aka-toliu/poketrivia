61a724a061ea810b7ea9f3cc489f55de77696e9e
Tulio Santos <tulio.kprod@gmail.com>
2024-05-02 12:50:21 -0300

    structure: creating main routers path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/play/play.component';
+import { RankedComponent } from './pages/ranked/ranked.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'play', title: 'Play',  component: PlayComponent},
+    { path: 'ranked', title: 'Ranked',  component: RankedComponent},
+    { path: '', title: 'Poketrivia', component: HomeComponent},
+];
