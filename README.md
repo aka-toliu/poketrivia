6f0aee48cb07a81a46135567b419f68777255154
Túlio <edd.di@unip.br>
2024-05-02 00:50:35 -0300

    refactor: update project to angular 17 + restart development

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
-# Poketrivia
+# V2
 
-▶️ Play here: https://poke-trivia.vercel.app/
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.6.
 
-![alt text](https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/75a331165877161.640f352a3e962.png)
+## Development server
 
-Share ideas, reports and critics here: [Poketriva discussions](https://github.com/byToliu/poketrivia/discussions) 
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-## Usage
+## Code scaffolding
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.0.
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-### Prerequisites
+## Build
 
-Run `npm install`
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-### Development server
+## Running unit tests
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. 
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-### Further help
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+
+## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,122 +1,13 @@
 import { Component } from '@angular/core';
-import { PoketriviaService } from './poketrivia.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
-  public allPokemon: any;
-  public pokebox: any = [];
-  public randomPokemon: any;
-  public initPokemon: any;
-  public life: number = 3;
-  public points: number = 0;
-  public buttonAction: boolean = false;
-  public pokemonGeneration: string = "151";
-  public started: boolean = false;
-  public highscore: any;
-  public finished: boolean = false;
-  public help: boolean = false;
-  public loading: boolean = false;
-
-
-
-
-  constructor(private poketriviaService: PoketriviaService) {
-
-  }
-
-  ngOnInit(): void {
-
-    let highscore = localStorage.getItem('highscore');
-    if (highscore !== null) {
-      this.poketriviaService.highscore = highscore;
-    } else {
-      this.poketriviaService.highscore = 0;
-    }
-
-  }
-
-  getPokemons() {
-
-    this.poketriviaService.getAll(this.pokemonGeneration).subscribe((pokemons) => {
-
-
-      this.poketriviaService.pokemon = pokemons.results;
-      this.pokebox = this.poketriviaService.pokebox;
-      this.poketriviaService.randomizeInitPokemon();
-      this.poketriviaService.randomizePokemon();
-      this.randomPokemon = this.poketriviaService.randomPokemon;
-      this.initPokemon = this.poketriviaService.initPokemon;
-      this.loading = false;
-
-    })
-    this.poketriviaService.newpokemon$.subscribe(res => {
-      this.randomPokemon = res
-    })
-
-    this.poketriviaService.newlife$.subscribe(res => {
-      this.life = res
-    })
-
-    this.poketriviaService.newpoints$.subscribe(res => {
-      this.points = res
-    })
-
-    this.poketriviaService.newhighscore$.subscribe(res => {
-      this.highscore = res
-    })
-
-    this.poketriviaService.newfinished$.subscribe(res => {
-      this.finished = res
-    })
-
-  }
-
-
-  shareResults() {
-    let result = "⭕ https://poke-trivia.vercel.app | 🥇 My score: " + this.points + " 🏆 Highscore: " + + this.highscore + " | #Poketrivia";
-
-    navigator.clipboard.writeText(result);
-
-
-    setTimeout(() => {
-      this.buttonAction = false;
-    }, 2500);
-  }
-
-
-  start() {
-
-    if (this.started == false) {
-      this.getPokemons();
-    }
-
-  }
-
-  updateLimit() {
-    this.poketriviaService.limit = this.pokemonGeneration;
-  }
-
-  refresh(): void {
-    this.poketriviaService.pokemon = [];
-    this.poketriviaService.pokebox = [];
-    this.poketriviaService.randomPokemon = {};
-    this.randomPokemon = {};
-    this.allPokemon = undefined;
-    this.pokebox = undefined;
-    this.poketriviaService.life = 3;
-    this.poketriviaService.points = 0;
-    this.life = 3;
-    this.points = 0;
-    this.finished = false;
-    this.loading = true;
-
-    this.getPokemons();
-
-  }
-
+  title = 'v2';
 }
