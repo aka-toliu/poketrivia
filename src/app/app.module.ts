6a6c278e6f0074402bd3760f083197383795f4c7
Tulio Santos <tulio.kprod@gmail.com>
2024-05-09 12:57:01 -0300

    style: main visual of home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
+import { RouterModule } from '@angular/router';
+
+
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    RouterModule,
     AngularFireModule.initializeApp(environment.firebase),
   ]
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
